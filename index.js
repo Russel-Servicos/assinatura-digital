9184f740863ecd1f25edbe897900582652cd7d5a
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2023-12-07 10:00:47 -0300

    feat: adiciona logs, env, criacao pasta, delecao de imagem, geracao de nome da imagem

--- index.js
+++ index.js
@@ -1,30 +1,46 @@
 const express = require("express");
-const nodeHtmlToImage = require("node-html-to-image");
-const path = require("path");
+const htmlToImage = require("node-html-to-image");
+const crypto = require("node:crypto");
+const path = require("node:path");
 const fs = require("node:fs");
 
 const app = express();
-const port = 3000;
+const port = 24066;
 
-const pathIndex = path.join(__dirname, "public", "index.html");
+const env = process.env.NODE_ENV;
+const isProduction = env === "production";
 
-const pathAssinaturaDigital = path.join(
+const publicPath = path.join(__dirname, "public");
+
+const assinaturaDigitalPath = path.join(
   __dirname,
   "public",
   "assinatura-digital.html"
 );
+
+let tmpPath = path.join(__dirname, "tmp");
+if (isProduction) {
+  tmpPath = path.join("/tmp");
+} else {
+  tmpPath = path.join(__dirname, "tmp");
+  if (!fs.existsSync(tmpPath)) {
+    fs.mkdirSync(tmpPath);
+    console.log("Criado a pasta: " + tmpPath);
+  }
+}
 
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
+  console.log("Buscando página: " + publicPath + "/index.html");
+
   res.set("Content-Type", "text/html");
-  res.sendFile(pathIndex);
+  res.sendFile(publicPath + "/index.html");
 });
 
 app.post("/download", async (req, res) => {
-  console.log(req.body);
   const { name, career, phone, email } = req.body;
 
   const data = {
@@ -34,17 +50,31 @@ app.post("/download", async (req, res) => {
     email,
   };
 
-  const htmlContent = fs.readFileSync(pathAssinaturaDigital, "utf8");
-  const image = await nodeHtmlToImage({
+  console.log(`Buscando path da assinatura: ` + assinaturaDigitalPath);
+  const htmlContent = fs.readFileSync(assinaturaDigitalPath, "utf8");
+
+  const imageName = crypto.randomBytes(10).toString("hex") + ".png";
+  console.log(`Nome da imagem gerada: ${imageName}`);
+
+  const imagePath = path.join(tmpPath, imageName);
+  console.log(`Path da imagem: ${imagePath}`);
+
+  console.log(`Gerando a imagem...`);
+  const image = await htmlToImage({
     html: htmlContent,
+    output: path.join(tmpPath, imageName),
     content: data,
   });
+  console.log(`Imagem gerada.`);
 
-  res.status(200).format({
-    "image/png": () => res.send(image),
+  res.download(path.join(tmpPath, imageName), imageName, function (error) {
+    if (!error && !isProduction) {
+      fs.rmSync(imagePath);
+      console.log("Imagem removida (após o download): " + imagePath);
+    }
   });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Rodando na porta ${port}`);
 });
