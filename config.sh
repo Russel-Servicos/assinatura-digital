c0b6e7f7fe04ffa3faf23efe267b19d3d6a3a868
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2023-12-13 17:39:38 -0300

    config.sh para instalar dependencias

---
+++ config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+apt update -y && 
+apt install -y curl net-tools sudo && 
+pip install -r requirements.txt &&
+apt install -y libqt5webkit5-dev libqt5xmlpatterns5-dev libqt5svg5-dev && 
+wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-2/wkhtmltox_0.12.6.1-2.bullseye_amd64.deb && 
+dpkg -i wkhtmltox_0.12.6.1-2.bullseye_amd64.deb ; 
+apt --fix-broken install -y && 
+dpkg -i wkhtmltox_0.12.6.1-2.bullseye_amd64.deb
