697e85861164d61c711ec998262d7ccef7651857
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2024-08-27 10:37:51 -0300

    especifica versao no requirements

--- config-container.sh
+++ config-container.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt update -y && 
-apt install -y curl net-tools sudo && 
+apt install -y curl net-tools && 
 pip install -r requirements.txt &&
 apt install -y libqt5webkit5-dev libqt5xmlpatterns5-dev libqt5svg5-dev && 
 wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-2/wkhtmltox_0.12.6.1-2.bullseye_amd64.deb && 
